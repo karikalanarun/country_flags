4e854fb2612a58cfd0b051f02ab03a34ac81390d
Arun Karikalan <arun.k@skitter.in>
2020-02-07 11:40:35 +0530

    axios setup. the basic ui.

--- compiled/app.js
+++ compiled/app.js
@@ -1,2 +1,9 @@
 "use strict";
-console.log("Hello World!!!");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var axios_1 = __importDefault(require("axios"));
+axios_1.default.get("https://restcountries.eu/rest/v2/all").then(function (data) {
+    return console.log(data);
+});
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,5 @@
-console.log("Hello World!!!");
+import Axios from "axios";
+
+Axios.get("https://restcountries.eu/rest/v2/all").then(data =>
+  console.log(data)
+);
